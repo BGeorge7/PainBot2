1179d59efb002c33f86043f7f367b55bfa2601cf
George Barroso <omr0reoo@gmail.com>
2020-12-28 18:56:34 -0500

    Added feature to ignore plain responses if the beta bot is online and let the beta bot take over

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const Commando = require('discord.js-commando');
 const config = require('./config.json');
 const plainResponses = require("./responses/respond.js");
 
+let selfID
+
 config.mainDir = __dirname;
 
 const client = new Commando.CommandoClient({
@@ -15,14 +17,7 @@ const client = new Commando.CommandoClient({
 
 client.on('ready', async () => {
   console.log('The client is ready!');
-
-  client.user.setPresence({
-    game: { 
-        name: 'VS Code',
-        type: 'Waiting for Frank to post the shitty meme again'
-    },
-  })
-
+  
   client.registry
     .registerGroups([
       ['misc', 'misc commands'],
@@ -32,11 +27,35 @@ client.on('ready', async () => {
     .registerDefaults()
     .registerCommandsIn(path.join(__dirname, 'cmds')); //importing of the commands folder
 
+  selfID = client.user.id 
+  if(selfID === config.BetaBranchID)
+  {
+    console.log("Running on the beta branch")
+    client.user.setActivity("VSCode and Pain");
+  }
+  else if(selfID === config.ProductionBranchID)
+  {
+    console.log("Running on the production branch")
+    client.user.setActivity("Type -help");
+  }
+
 });
 
 client.on('message', async message => { // for non prefix commands\
 
+
     if(message.content.startsWith(config.prefix) || message.author.bot) return;
+
+    if(selfID === config.ProductionBranchID && message.guild.member(config.BetaBranchID).presence.status ==! "offline")
+    {
+      console.log("Beta branch is online.\nIgnoring message...")
+      return;
+    }
+
+    //console.log(client.users.cache.get(config.ProductionBranchID).presence.status); //<- This is how you read info on a user that the bot is in a server with
+    //console.log(message.guild.member(config.ProductionBranchID).presence.status) // <- this one is only works on the particular guild that the message was sent on
+
+    //-----------Code to run goes here--------------//
     plainResponses.run(message, config.mainDir);
 });
 
